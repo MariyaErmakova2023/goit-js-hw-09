65edcfe4e6468d97d834a86501e2d08ab17431f7
MariyaErmakova2023 <ermakova.mariya2017@gmail.com>
2024-06-10 00:26:34 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,30 +2,34 @@ const STORAGE_KEY = 'feedback-form-state';
 
 const formData = { email: '', message: '' };
 const form = document.querySelector('.feedback-form');
-const input = document.querySelector('email');
-const message = document.querySelector('textarea');
+const input = form.querySelector('input[name="email"]');
+const message = form.querySelector('textarea[name="message"]');
 
 form.addEventListener('input', () => {
   const formData = new FormData(form);
   const email = formData.get('email');
   const message = formData.get('message');
+  const data = { email, message };
   saveToLS('email', email);
   saveToLS('message', message);
+  saveToLS(STORAGE_KEY, data);
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-  const email = loadFromLS('email');
-  const message = loadFromLS('message');
-  form.elements.email.value = email;
-  form.elements.message.value = message;
+  // const email = loadFromLS('email');
+  // const message = loadFromLS('message');
+  const savedData = loadFromLS(STORAGE_KEY);
+  // console.log(savedData);
+  form.elements.email.value = savedData.email;
+  form.elements.message.value = savedData.message;
 });
 
 form.addEventListener('submit', event => {
   event.preventDefault();
 
   const formData = new FormData(form);
-  const email = formData.get('email');
-  const message = formData.get('message');
+  const email = formData.get('email').trim();
+  const message = formData.get('message').trim();
 
   if (!email || !message) {
     alert('Fill please all fields');
@@ -34,8 +38,8 @@ form.addEventListener('submit', event => {
     console.log(data);
 
     form.reset();
-    localStorage.removeItem('email');
-    localStorage.removeItem('message');
+    localStorage.removeItem(STORAGE_KEY);
+    // localStorage.removeItem('message');
   }
 });
 
